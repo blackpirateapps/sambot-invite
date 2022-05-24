96e87a89924a2ced842bb7ec24e91358e5574a38
blackpirateapps <blackpirate101@protonmail.com>
2022-05-24 16:55:03 +0530

    added js to show conformation

---
+++ script.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById("success-js").innerHTML = "Your form has been submitted successfully"
+    document.getElementById("success-hidden").style.display = "none"
+}
